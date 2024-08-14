85f32e36543e6835c9f92771ca75862d4e924585
guitardev <guitar.devmode@gmail.com>
2024-08-14 14:11:52 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,23 @@
         <p>
           นี่คือหน้าหลักของแอปพลิเคชันของคุณ คุณสามารถเริ่มสร้างเนื้อหาและฟีเจอร์ต่างๆ ได้ที่นี่
         </p>
+        <v-btn color="primary">Click me!</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const drawer = ref(false);
+
+    return {
+      drawer
+    };
+  }
+}
+</script>
